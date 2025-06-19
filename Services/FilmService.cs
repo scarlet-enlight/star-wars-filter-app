8f5ea7373b631bd289c23bc1dd41ec4e72ddebe0
blaze <blazejostaszewski@gmail.com>
2025-06-19 22:08:42 +0200

    Dodano klasę FilmService do zarządzania filmami
    Wprowadzono klasę `FilmService`, która obsługuje operacje związane z filmami, w tym metody `GetAllFilms` do pobierania wszystkich filmów oraz `GetFilmsByTitles` do wyszukiwania filmów według tytułu. Klasa korzysta z `MySQLConnectionService` do nawiązywania połączenia z bazą danych oraz `MySqlCommand` i `MySqlDataReader` do wykonywania zapytań SQL i odczytu wyników.

---
+++ Services/FilmService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StarWarsFilterApp.Model;
+using MySql.Data.MySqlClient;
+
+namespace StarWarsFilterApp.Services
+{
+    class FilmService
+    {
+        private readonly MySQLConnectionService _connectionService;
+
+        public FilmService()
+        {
+            _connectionService = new MySQLConnectionService();
+        }
+
+        public List<Film> GetAllFilms()
+        {
+            var films = new List<Film>();
+            using (var conn = _connectionService.GetConnection())
+            {
+                string query = "SELECT id, title FROM films";
+                using (var cmd = new MySqlCommand(query, conn))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        films.Add(new Film
+                        {
+                            Id = reader.GetInt32("id"),
+                            Title = reader.GetString("title"),
+                        });
+                    }
+                }
+            }
+            return films;
+        }
+
+        public List<Film> GetFilmsByTitles(string title)
+        {
+            var films = new List<Film>();
+            using (var conn = _connectionService.GetConnection())
+            {
+                string query = "SELECT id, title FROM films WHERE title = @title";
+                using (var cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@title", title);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            films.Add(new Film
+                            {
+                                Id = reader.GetInt32("id"),
+                                Title = reader.GetString("title")
+                            });
+                        }
+                    }
+                }
+            }
+            return films;
+        }
+    }
+}
