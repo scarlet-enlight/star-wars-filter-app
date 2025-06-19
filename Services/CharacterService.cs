fd2989d54cbc7e7019f4143c50d67ae846d2f823
Maxim <starlitskycode@gmail.com>
2025-06-19 20:44:52 +0200

    Add CharacterService for MySQL character retrieval
    Introduces the `CharacterService` class in the `StarWarsFilterApp.Services` namespace. This class provides methods to fetch all characters and filter them by species from a MySQL database. It utilizes `MySQLConnectionService` for database connections and `MySqlCommand` for executing SQL queries. Additional using directives have been added as needed.

---
+++ Services/CharacterService.cs
@@ -0,0 +1,70 @@
+﻿using StarWarsFilterApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+
+namespace StarWarsFilterApp.Services
+{
+    class CharacterService
+    {
+        private readonly MySQLConnectionService _connectionService;
+
+        public CharacterService()
+        {
+            _connectionService = new MySQLConnectionService();
+        }
+
+        public List<Character> GetAllCharacters()
+        {
+            var characters = new List<Character>();
+            using (var conn = _connectionService.GetConnection())
+            {
+                string query = "SELECT id, name FROM characters";
+                using (var cmd = new MySqlCommand(query, conn))
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        characters.Add(new Character
+                        {
+                            Id = reader.GetInt32("id"),
+                            Name = reader.GetString("name"),
+                        });
+                    }
+                }
+            }
+            return characters;
+        }
+
+        public List<Character> GetCharactersBySpecies(string species)
+        {
+            var characters = new List<Character>();
+            using (var conn = _connectionService.GetConnection())
+            {
+                string query = "SELECT id, name, species FROM characters WHERE species = @species";
+                using (var cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@species", species);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            characters.Add(new Character
+                            {
+                                Id = reader.GetInt32("id"),
+                                Name = reader.GetString("name"),
+                                Species = reader.GetString("species")
+                            });
+                        }
+                    }
+                }
+            }
+            return characters;
+        }
+    }
+}
