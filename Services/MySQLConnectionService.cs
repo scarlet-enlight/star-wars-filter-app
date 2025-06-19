9bdcb09bc3195f863b84227487a63241c5e3d537
Maxim <starlitskycode@gmail.com>
2025-06-19 20:45:06 +0200

    Add MySQLConnectionService for database access
    Introduces a new class `MySQLConnectionService` in the
    `StarWarsFilterApp.Services` namespace. This class contains
    a private connection string for a MySQL database and a
    method `GetConnection()` to establish and open a database
    connection using the MySql.Data.MySqlClient library.

---
+++ Services/MySQLConnectionService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+
+namespace StarWarsFilterApp.Services
+{
+    public class MySQLConnectionService
+    {
+        private readonly string _connStr = "server=127.0.0.1;user=root;database=star_wars;port=3306;password=root";
+
+        public MySqlConnection GetConnection()
+        {
+            var conn = new MySqlConnection(_connStr);
+            conn.Open();
+            return conn;
+        }
+    }
+}
